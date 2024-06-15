07651e3bce3b3a9c3e91076492a70f97ae3ffcc6
Daniel <dflyboy420@gmail.com>
2024-06-15 03:19:22 +0200

    create events

--- lib/video_sync_web/channels/room_channel.ex
+++ lib/video_sync_web/channels/room_channel.ex
@@ -14,28 +14,26 @@ defmodule VideoSyncWeb.RoomChannel do
   # by sending replies to requests from the client
   @impl true
   def handle_in("play", _, socket) do
-    broadcast!(socket, "play")
+    broadcast!(socket, "play", %{time: socket.assigns.time})
     {:noreply, assign(socket, :playing, true)}
   end
 
   @impl true
   def handle_in("pause", _, socket) do
-    broadcast!(socket, "pause")
+    broadcast!(socket, "pause", %{time: socket.assigns.time})
     {:noreply, assign(socket, :playing, false)}
   end
 
   @impl true
-  def handle_event("add_video", %{"url" => url}, socket) do
-    broadcast!(socket, "add_video", %{url: url})
-    {:noreply, assign(socket, video_url: url)}
+  def handle_in("timeupdate", %{"time" => time}, socket) do
+    broadcast!(socket, "timeupdate", %{time: time})
+    {:noreply, assign(socket, :time, time)}
   end
 
-  # It is also common to receive messages from the client and
-  # broadcast to everyone in the current topic (room:lobby).
   @impl true
-  def handle_in("shout", payload, socket) do
-    broadcast(socket, "shout", payload)
-    {:noreply, socket}
+  def handle_in("add_video", %{"url" => url}, socket) do
+    broadcast!(socket, "add_video", %{url: url})
+    {:noreply, assign(socket, video_url: url, playing: false, time: 0)}
   end
 
   # Add authorization logic here as required.
