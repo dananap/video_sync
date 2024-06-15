0233febad812331290ee123710b263d4b18547c4
Daniel <dflyboy420@gmail.com>
2024-06-15 16:07:24 +0200

    fix router

--- lib/video_sync_web/router.ex
+++ lib/video_sync_web/router.ex
@@ -16,12 +16,8 @@ defmodule VideoSyncWeb.Router do
 
   scope "/", VideoSyncWeb do
     pipe_through :browser
-    live "/rooms", RoomLive.Index, :index
-    live "/rooms/new", RoomLive.Index, :new
-    live "/rooms/:id/edit", RoomLive.Index, :edit
 
-    live "/rooms/:id", RoomLive.Show, :show
-    live "/rooms/:id/show/edit", RoomLive.Show, :edit
+    get "/", PageController, :home
   end
 
   # Other scopes may use custom stacks.
